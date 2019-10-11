88aac83f821c740d3dd1d280273a52eef6fe98bb
ayseirmak <ayseirmakercevik@gmail.com>
2019-10-11 21:06:21 +0300

    Getting ready for deployment to heroku

--- src/test/java/com/organization/name/myProject/AppTest.java
+++ src/test/java/com/organization/name/myProject/AppTest.java
@@ -1,6 +1,6 @@
 package com.organization.name.myProject;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import java.util.ArrayList;
@@ -14,18 +14,18 @@ public class AppTest
 {
 	public void testFound() {
 		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-		assertTrue(new App().search(array, 4));
+		assertTrue(new App().double_search(array, 4));
 		}
 		public void testNotFound() {
 		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-		assertFalse(new App().search(array, 5));
+		assertFalse(new App().double_search(array, 5));
 		}
 		public void testEmptyArray() {
 		ArrayList<Integer> array = new ArrayList<>();
-		assertFalse(new App().search(array, 1));
+		assertFalse(new App().double_search(array, 1));
 		}
 		public void testNull() {
-		assertFalse(new App().search(null, 1));
+		assertFalse(new App().double_search(null, 1));
 		}
 
     public void shouldAnswerWithTrue()
